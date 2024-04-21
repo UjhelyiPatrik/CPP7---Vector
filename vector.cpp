38cd07f703d59fc7682b475d353c554c78874652
Ujhelyi Patrik <ujhelyi.patrik1011@gmail.com>
2024-04-21 22:20:46 +0200

    Created constructors and destructor

--- vector.cpp
+++ vector.cpp
@@ -6,3 +6,19 @@
  */
 
 #include "vector.hpp"
+
+Vector::Vector(): pData(nullptr), elementNum(0) {}
+
+Vector::Vector(const Vector& other) {
+
+	elementNum = other.elementNum;
+	pData = new int[elementNum];
+
+	for (int i = 0; i < elementNum; i++){
+		this->pData[i] = other.pData[i];
+	}
+}
+
+Vector::~Vector() {
+	delete[]pData;
+}
